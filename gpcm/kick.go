218fff0138f5f5a7971c98b2a7ab995ce180769f
Pancakes <patapancakes@pagefault.games>
2026-07-26 22:44:57 -0400

    Remove unused KickPlayerCustomMessage func

--- gpcm/kick.go
+++ gpcm/kick.go
@@ -16,16 +16,3 @@ func KickPlayer(profileID uint32, reason string) {
 
 	kickPlayer(profileID, reason)
 }
-
-func KickPlayerCustomMessage(profileID uint32, reason string) {
-	mutex.Lock()
-	defer mutex.Unlock()
-
-	if session, exists := sessions[profileID]; exists {
-		session.replyError(GPError{
-			ErrorCode:   ErrConnectionClosed.ErrorCode,
-			ErrorString: "The player was kicked from the server. Reason: " + reason,
-			Fatal:       true,
-		})
-	}
-}
