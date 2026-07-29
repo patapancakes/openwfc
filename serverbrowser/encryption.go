07cf718a0e15e32b6346d96ff4c04651ee05bdd6
Pancakes <patapancakes@pagefault.games>
2026-07-28 21:00:02 -0400

    Tweak ServerBrowser encryption functions more

--- serverbrowser/encryption.go
+++ serverbrowser/encryption.go
@@ -64,7 +64,7 @@ func (s *CryptState) init(challenge []byte) {
 	s.ratchet = s.cards[3]
 	s.avalanche = s.cards[5]
 	s.lastPlain = s.cards[7]
-	s.lastCipher = s.cards[rsum%len(s.cards)]
+	s.lastCipher = s.cards[rsum]
 }
 
 func (s *CryptState) keyrand(limit int, key []byte, rsum, keyPos int) (int, int, int) {
@@ -73,34 +73,29 @@ func (s *CryptState) keyrand(limit int, key []byte, rsum, keyPos int) (int, int,
 	}
 
 	mask := 1
-	doLoop := true
-	if limit > 1 {
-		for doLoop {
-			mask = (mask << 1) + 1
-			doLoop = mask < limit
-		}
+	for mask < limit {
+		mask = (mask << 1) | 1
 	}
 
-	retries := 0
 	u := 0
-	doLoop = true
-	for doLoop {
-		rsum = int(s.cards[rsum%len(s.cards)] + key[keyPos])
+	for retries := 0; ; retries++ {
+		rsum = int(s.cards[rsum]+key[keyPos]) % len(s.cards)
 		keyPos++
 
 		if keyPos >= len(key) {
 			keyPos = 0
-			rsum += len(key)
+			rsum = (rsum + len(key)) % len(s.cards)
 		}
 
 		u = rsum & mask
 
-		retries++
-		if retries > 11 {
+		if retries >= 11 {
 			u %= limit
 		}
 
-		doLoop = u > limit
+		if u <= limit {
+			break
+		}
 	}
 
 	return u, rsum, keyPos
@@ -115,55 +110,25 @@ func (s *CryptState) Encrypt(data []byte) []byte {
 }
 
 func (s *CryptState) encryptByte(d byte) byte {
-	a := s.rotor
-	b := s.ratchet
-	c := s.cards[a]
-	s.rotor = a + 1
-	s.ratchet = b + c
+	s.ratchet += s.cards[s.rotor]
+	s.rotor++
 
-	a = s.lastCipher
-	b = s.ratchet
-	b = s.cards[b]
-	c = s.cards[a]
-	s.cards[a] = b
+	swaptemp := s.cards[s.lastCipher]
 
-	a = s.lastPlain
-	b = s.ratchet
-	a = s.cards[a]
-	s.cards[b] = a
+	s.cards[s.lastCipher] = s.cards[s.ratchet]
+	s.cards[s.ratchet] = s.cards[s.lastPlain]
+	s.cards[s.lastPlain] = s.cards[s.rotor]
+	s.cards[s.rotor] = swaptemp
 
-	a = s.rotor
-	b = s.lastPlain
-	a = s.cards[a]
-	s.cards[b] = a
+	s.avalanche += s.cards[swaptemp]
 
-	a = s.rotor
-	s.cards[a] = c
+	first := s.cards[s.cards[s.avalanche]+s.cards[s.rotor]]
+	second := s.cards[s.cards[s.cards[s.lastPlain]+s.cards[s.lastCipher]+s.cards[s.ratchet]]]
 
-	b = s.avalanche
-	a = s.cards[c]
-	c = s.lastPlain
-	b = a + b
-	s.avalanche = b
+	ciphertext := d ^ first ^ second
 
-	a = b
-	c = s.cards[c]
-	b = s.ratchet
-	b = s.cards[b]
-	a = s.cards[a]
-	c = b + c
-	b = s.lastCipher
-	b = s.cards[b]
-	c = b + c
-	b = s.cards[c]
-	c = s.rotor
-	c = s.cards[c]
-	a = a + c
-	c = s.cards[b]
-	b = s.cards[a]
-	c ^= b ^ d
-	s.lastCipher = c
+	s.lastCipher = ciphertext
 	s.lastPlain = d
 
-	return c
+	return ciphertext
 }
